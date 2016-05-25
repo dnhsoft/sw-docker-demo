08c5f70586b235a992e3271125c84802d733e407
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2016-05-25 17:12:39 +0300

    Plugin automatically installed and activated on init.sh

--- 06-develop-a-plugin/.docker/init.sh
+++ 06-develop-a-plugin/.docker/init.sh
@@ -2,3 +2,7 @@
 
 docker-compose exec shop /swtools/init.sh
 
+# install and activate the Slogan of the day plugin
+docker-compose exec shop php bin/console sw:plugin:refresh
+docker-compose exec shop php bin/console sw:plugin:install SwagSloganOfTheDay
+docker-compose exec shop php bin/console sw:plugin:activate SwagSloganOfTheDay
