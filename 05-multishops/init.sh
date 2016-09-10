28f559b5d3ad4b7d10f502d0549f8362c7535da9
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2016-09-10 12:07:23 +0300

    Fixed forgotten comment in multishops

--- 05-multishops/init.sh
+++ 05-multishops/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#docker-compose exec shop513 /swtools/init.sh
-#docker-compose exec shop515 /swtools/init.sh
+docker-compose exec shop513 /swtools/init.sh
+docker-compose exec shop515 /swtools/init.sh
 docker-compose exec shop524 /swtools/init.sh
 docker-compose exec shop526 /swtools/init.sh
